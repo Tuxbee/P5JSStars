516e7e356719f8a0423c5efb9b97ddb991bf94f3
Tuxbee <tuxbe@hotmail.com>
2019-11-14 20:39:42 +0100

    Add project files.

---
+++ P5JSStars/js/Star.js
@@ -0,0 +1,46 @@
+﻿// Daniel Shiffman
+// http://codingtra.in
+// http://patreon.com/codingtrain
+// Code for: https://youtu.be/17WoOqgXsRM
+
+function Star(initial_b) {
+   this.x = 0;
+   this.y = 0;
+   this.z = 0;
+   this.myid = initial_b ? 0 : -1;
+
+   this.setup = function (starID)
+   {
+      this.x = random(-width, width);
+      this.y = random(-height, height);
+      this.z = random(width);
+
+      if (this.myid === 0)
+         return (new Star(false)).setup(starID);
+      else {
+         this.myid = starID;
+         starID = starID - 1;
+         if (starID > 0)
+            return [this].concat((new Star(false)).setup(starID));
+         else
+            return [this];
+      }
+   }
+
+   this.affiche = function () {
+      this.z = this.z - speed;
+      if (this.z < 1) {
+         this.z = width;
+         this.x = random(-width, width);
+         this.y = random(-height, height);
+      }
+      fill(255);
+      noStroke();
+
+      var sx = map(this.x / this.z, 0, 1, 0, width);
+      var sy = map(this.y / this.z, 0, 1, 0, height);
+
+      var r = map(this.z, 0, width, 16, 0);
+      ellipse(sx + (width / 2), sy + (height / 2), r, r);
+   }
+}
---
+++ P5JSStars/js/sketch.js
@@ -0,0 +1,30 @@
+﻿// Daniel Shiffman
+// http://codingtra.in
+// http://patreon.com/codingtrain
+// Code for: https://youtu.be/17WoOqgXsRM
+
+let stars = [];
+let starID;
+let speed;
+
+function setup() {
+   createCanvas(600, 600);
+
+   starID = 800;
+   stars = (new Star(true)).setup(starID);
+
+   /*
+   for (let i = 0; i < 800; i++) {
+      stars[i] = new Star();
+   }
+   */
+}
+
+function draw() {
+   speed = map(mouseX, 0, width, 0, 50);
+   background(0);
+
+   for (let i = 0; i < stars.length; i++) {
+      stars[i].affiche();
+   }
+}
